575d953e8c3cf013664f480da5663a59bd116803
JaloliddinKulmamatov <itjalol2010@gmail.com>
2024-05-26 22:00:48 +0500

    first commit

---
+++ app.ts
@@ -0,0 +1,101 @@
+//1
+//   function checkAge(age:number) :boolean {
+//     if (age > 18) {
+//       return true;
+//     } else {
+//       return confirm('Did parents allow you?');
+//     }
+//   }
+
+//Ha muhim
+
+//2
+
+// function min(num1:number,num2:number):number {
+//     return Math.min(num1,num2)
+// }
+//  function min(num1:number,num2:number):number {
+//     if (num1 > num2) {
+//         return num2
+//     }else if (num1 < num2) {
+//         return num1
+//     }
+// }
+
+// console.log(min(2,5));
+
+
+//3
+
+// function pow(nums:number,degree:number):number {
+//     return nums ** degree
+// }
+
+// function pow(nums:number,degree:number):number {
+//     return Math.pow(nums,degree);
+// }
+
+// console.log(pow(2,5));
+
+//4
+
+// function calcAge(year:number) {
+//     return year * 365
+// }
+
+// console.log(calcAge(20));
+
+//5
+
+// function cubes(a:number) {
+// 	return a ** 3
+// }
+//BU yerda 2ta hatolik bor edi.1 a g type berilmaga.2-return emas retunr yozilgan edi
+
+//6
+
+// function circuitPower(num1:number , num2:number) {
+//     return num1 * num2
+// }
+
+// function calculatePower(voltage: number, current: number): number {
+//     // Quvvat = Voltaj * O'qim
+//     const power = voltage * current;
+//     return power;
+// }
+
+// console.log(circuitPower(480, 20));
+
+//7
+
+// function howManySeconds(num1:number) {
+//    return num1 * 3600
+// }
+
+// console.log(howManySeconds(10) );
+
+//8
+
+// function remainder(number1:number , number2:number) {
+//     return number1 % number2
+// }
+
+// console.log(remainder(3 , 4));
+
+
+//9
+// function giveMeSomething(str : string):string {
+//     return ` something ${str }`
+// }
+// console.log(giveMeSomething("Bob Jane"));
+
+//10
+
+// function lessThanOrEqualToZero(num:number) {
+//     if (num <= 0) {
+//         return false
+//     }else {
+//         return true
+//     }
+// }
+// console.log(lessThanOrEqualToZero(5));
